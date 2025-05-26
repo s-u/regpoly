0a6b107a4a5730d37635914a65676338f129e008
Simon Urbanek <simon.urbanek@r-project.org>
2025-05-26 17:49:17 +1200

    bugfix: correct number of edges

--- R/regpoly.R
+++ R/regpoly.R
@@ -1,7 +1,7 @@
 regpoly <-
 function(n=3, r=1, x=0, y=0, plot=TRUE, add=FALSE, ...) {
     validate.args(n, r)
-    phi <- seq(0, 2*pi, length = n)
+    phi <- seq(0, 2*pi, length = n + 1)
     phi <- phi[-length(phi)]
     x <- x + r * sin(phi)
     y <- y + r * cos(phi)
