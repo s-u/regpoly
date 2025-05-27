2e72f3fc5784d3442aa42285feceec99cd90cbf1
Simon Urbanek <simon.urbanek@r-project.org>
2025-05-27 15:13:51 +1200

    Fix regression when changing r to rx/ry

--- R/regpoly.R
+++ R/regpoly.R
@@ -1,6 +1,7 @@
 regpoly <-
 function(n=3, rx=1, ry=1, x=0, y=0, plot=TRUE, add=FALSE, ...) {
-    validate.args(n, r)
+    validate.args(n, rx)
+    validate.args(n, ry)
     phi <- seq(0, 2*pi, length = n + 1)
     phi <- phi[-length(phi)]
     x <- x + rx * sin(phi)
